b59fa60da5ac84d39f7820cc521e776f5b030651
Shishir420-GIT <shishirsrivastav@gmail.com>
2024-01-26 17:37:05 +0530

    Adding counter logic

---
+++ counter/src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [counter, setCounter] = useState(10)
+  //let counter = 11
+  const addValue = () => {
+    //These are in batch won't work as react sees it a a single task
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    // setCounter(counter + 1)
+    setCounter((preVal) => preVal +1 )
+    setCounter((preVal) => preVal +1 )
+    setCounter((preVal) => preVal +1 )
+    setCounter((preVal) => preVal +1 )
+  }
+
+  const subValue = () => {
+    setCounter(counter - 1)
+  }
+  return (
+    <>
+    <h1>Learning React from Scratch</h1>
+    <h3>Current value of counter is {counter}</h3>
+    <button
+    onClick={addValue}
+    >Add Value</button> {" "}
+    <button onClick={subValue}>Sub Value</button>
+    </>
+  )
+}
+
+export default App
